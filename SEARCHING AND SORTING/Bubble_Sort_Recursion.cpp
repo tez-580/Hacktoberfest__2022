648e7dccfc449a932875c50ecc06376fa220f682
VasanthThallam <96721657+VasanthThallam@users.noreply.github.com>
2022-10-18 12:07:04 +0530

    Added Bubble Sort using Recursion in C++
    please add hacktober tag

---
+++ SEARCHING AND SORTING/Bubble_Sort_Recursion.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void bubblesort(int arr[], int n)
+{
+    if (n == 0 || n == 1)
+        return;
+    
+    for (int i = 0; i < n - 1; i++)
+        if (arr[i] > arr[i + 1])
+            swap(arr[i], arr[i + 1]);
+
+    bubblesort(arr, n - 1);
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    bubblesort(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+}
+
